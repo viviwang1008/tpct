691281772b3e65266c2e5893aad50e98360444a0
Wang Wei <weiwang15@student.ethz.ch>
2021-06-15 19:34:13 +0200

    Change output folder for 16 cf runs

--- runs_bci2000.sh
+++ runs_bci2000.sh
@@ -8,13 +8,13 @@ python main_train_bci2000.py -o bci2000_model_comparison/eegC32/ -m eegC -cf 32 
 python main_train_bci2000.py -o bci2000_model_comparison/eegD32S/ -m eegD -cf 32 --stride -e 80 -p 80
 python main_train_bci2000.py -o bci2000_model_comparison/eegD32/ -m eegD -cf 32 --no-stride -e 80 -p 80
 python main_train_bci2000.py -o bci2000_model_comparison/eegA32S/ -m eegA -cf 16 --stride -e 80 -p 80
-python main_train_bci2000.py -o bci2000_model_comparison/eegA32/ -m eegA -cf 16 --no-stride -e 80 -p 80
-python main_train_bci2000.py -o bci2000_model_comparison/eegB32S/ -m eegB -cf 16 --stride -e 80 -p 80
-python main_train_bci2000.py -o bci2000_model_comparison/eegB32/ -m eegB -cf 16 --no-stride -e 80 -p 80
-python main_train_bci2000.py -o bci2000_model_comparison/eegC32S/ -m eegC -cf 16 --stride -e 80 -p 80
-python main_train_bci2000.py -o bci2000_model_comparison/eegC32/ -m eegC -cf 16 --no-stride -e 80 -p 80
-python main_train_bci2000.py -o bci2000_model_comparison/eegD32S/ -m eegD -cf 16 --stride -e 80 -p 80
-python main_train_bci2000.py -o bci2000_model_comparison/eegD32/ -m eegD -cf 16 --no-stride -e 80 -p 80
+python main_train_bci2000.py -o bci2000_model_comparison/eegA16/ -m eegA -cf 16 --no-stride -e 80 -p 80
+python main_train_bci2000.py -o bci2000_model_comparison/eegB16S/ -m eegB -cf 16 --stride -e 80 -p 80
+python main_train_bci2000.py -o bci2000_model_comparison/eegB16/ -m eegB -cf 16 --no-stride -e 80 -p 80
+python main_train_bci2000.py -o bci2000_model_comparison/eegC16S/ -m eegC -cf 16 --stride -e 80 -p 80
+python main_train_bci2000.py -o bci2000_model_comparison/eegC16/ -m eegC -cf 16 --no-stride -e 80 -p 80
+python main_train_bci2000.py -o bci2000_model_comparison/eegD16S/ -m eegD -cf 16 --stride -e 80 -p 80
+python main_train_bci2000.py -o bci2000_model_comparison/eegD16/ -m eegD -cf 16 --no-stride -e 80 -p 80
 
 python main_train_bci2000.py -o bci2000_dropout_comparison/eegA0.1/ -dr 0.1 -e 80 -p 80
 python main_train_bci2000.py -o bci2000_dropout_comparison/eegA0.5/ -dr 0.5 -e 80 -p 80
